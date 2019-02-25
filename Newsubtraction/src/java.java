6e7bf841df14816e7d305b5005d6aa552131bec9
srikanthc296 <srikanthc296@github.com>
2019-02-25 10:28:20 -0500

    commit

---
+++ Newsubtraction/src/java.java
@@ -0,0 +1,14 @@
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a = 10;
+		b = 15;
+		c = a-b;
+		System.out.println(c);
+
+	}
+
+}
